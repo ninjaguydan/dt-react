1ea0bf8274f517a9d194b3270286f441ae806fe5
Daniel Thompson <dthompson0190@gmail.com>
2021-09-18 20:08:49 -0400

    started react app

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import Button from './Button'
+
+const Header = ({ title }) => {
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color = 'green' text='Add' />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+//---------- CSS in JS! ---------- //
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'black'
+// }
+
+export default Header
