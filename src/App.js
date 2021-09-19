046c71b30930c6de9f78483f8b682c28df67efd4
Daniel Thompson <dthompson0190@gmail.com>
2021-09-19 00:12:21 -0400

    added new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+        },
+    ]
+  )
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminger
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      { tasks.length > 0 ? (
+        <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder} />
+        ) : ( 'No Tasks!')
+      }
     </div>
   );
 }
