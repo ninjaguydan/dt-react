9540bd8c6a88e1a289b4629ddab9c812b80a6ee8
Daniel Thompson <dthompson0190@gmail.com>
2021-09-19 07:29:38 -0400

    added ability to create and delete tasks

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 function App() {
-  const [tasks, setTasks] = useState(
-    [
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Shopping',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-    ]
-  )
+	const [showAddTask, setShowAddTask] = useState(false)
+	const [tasks, setTasks] = useState(
+	[
+		{
+			id: 1,
+			text: 'Doctors Appointment',
+			day: 'Feb 5th at 2:30pm',
+			reminder: true,
+		},
+		{
+			id: 2,
+			text: 'Meeting',
+			day: 'Feb 5th at 2:30pm',
+			reminder: true,
+		},
+		{
+			id: 3,
+			text: 'Shopping',
+			day: 'Feb 5th at 2:30pm',
+			reminder: true,
+		},
+	]
+	)
+	// Add Task
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 10000) + 1
 
-  // Delete Task
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
+		const newTask = { id, ...task }
+		setTasks([...tasks, newTask])
+	}
+	// Delete Task
+	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id))
+	}
+	// Toggle Reminger
+	const toggleReminder = (id) => {
+		setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+	}
 
-  // Toggle Reminger
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
-  }
-
-  return (
-    <div className="container">
-      <Header />
-      { tasks.length > 0 ? (
-        <Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder} />
-        ) : ( 'No Tasks!')
-      }
-    </div>
-  );
+	return (
+		<div className="container">
+			<Header onAdd={ () => setShowAddTask(!showAddTask) } showAdd={showAddTask} />
+			{ showAddTask ? <AddTask onAdd={addTask} /> : '' }
+			{ tasks.length > 0 ? (
+				<Tasks tasks = {tasks} onDelete = {deleteTask} onToggle = {toggleReminder} />
+				) : ( 'No Tasks!')
+			}
+		</div>
+	);
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,9 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-    const onClick = (e) => {
-        console.log(e)
-    }
+const Button = ({ color, text, onClick }) => {
     return (
         <button 
-            onClick = { onClick } 
+            onClick={onClick}
             style = {{ backgroundColor: color }} 
             className = 'btn'>
                 { text }
